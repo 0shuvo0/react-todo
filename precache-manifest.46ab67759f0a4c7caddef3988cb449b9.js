a70a80826621cfb285270a490d5b00d5567686ee
0shuvo0 <seriouslolz1@gmail.com>
2020-08-02 06:26:31 +0000

    another url fix

---
+++ precache-manifest.46ab67759f0a4c7caddef3988cb449b9.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2eaf8b11ccd4d7762b7ace97f9a3fe0e",
+    "url": "/index.html"
+  },
+  {
+    "revision": "2b47dee33c9c8ea0dcdb",
+    "url": "/static/css/main.134dceb9.chunk.css"
+  },
+  {
+    "revision": "69527949195bba3d4673",
+    "url": "/static/js/2.56e57b59.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.56e57b59.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "2b47dee33c9c8ea0dcdb",
+    "url": "/static/js/main.4ee884f7.chunk.js"
+  },
+  {
+    "revision": "e2c6e531b8324ac17d6c",
+    "url": "/static/js/runtime-main.206ac5b9.js"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/react-todo/precache-manifest.48091e4cefec4e0b3fcb82c5cfc1757a.js"
+  "/precache-manifest.46ab67759f0a4c7caddef3988cb449b9.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/react-todo/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
   
   blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
